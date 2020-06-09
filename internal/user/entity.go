752686273b9e718f7d53c1bfe9c1b7af7945c0bd
adhistria <muhammad.satria@alodokter.com>
2020-06-09 22:00:37 +0700

    Add user package

--- internal/user/entity.go
+++ internal/user/entity.go
@@ -1,8 +1,8 @@
 package user
 
-type User struct{
-	ID uint64 
-	Name string `json:"name"`
-	Email string `json:"email"`
-	Password string `json:"password"`
+type User struct {
+	ID       uint64
+	Name     string `json:"name" db:"name"`
+	Email    string `json:"email" db:"email"`
+	Password string `json:"password" db:"password"`
 }
