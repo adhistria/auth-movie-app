5d514baec8dfa58f8c3b0a6365817fef6521fd55
adhistria <muhammad.satria@alodokter.com>
2020-06-18 21:08:28 +0700

    Delete unused code

--- internal/user/entity.go
+++
@@ -1,8 +0,0 @@
-package user
-
-type User struct {
-	ID       uint64
-	Name     string `json:"name" db:"name"`
-	Email    string `json:"email" db:"email"`
-	Password string `json:"password" db:"password"`
-}
