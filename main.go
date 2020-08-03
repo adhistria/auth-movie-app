328000afd885607eefc7e1dba3957b4a18a5af57
adhistria <muhammad.satria@alodokter.com>
2020-08-03 21:21:52 +0700

    Add main file

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"os"
 
+	"github.com/julienschmidt/httprouter"
 	log "github.com/sirupsen/logrus"
 
+	"net/http"
+
 	"github.com/adhistria/auth-movie-app/infrastructure/logs"
+	"github.com/adhistria/auth-movie-app/infrastructure/storage"
+	http_app "github.com/adhistria/auth-movie-app/internal/http"
+	"github.com/adhistria/auth-movie-app/internal/repository/psql"
+	"github.com/adhistria/auth-movie-app/internal/service"
 	"github.com/joho/godotenv"
 )
 
@@ -16,4 +23,17 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Trace(os.Getenv("DATABASE_URL"))
+
+	database, err := storage.NewConnection()
+	if err != nil {
+		log.Fatalf("Error init database connection : %s ", err)
+	}
+	router := httprouter.New()
+
+	userRepo := psql.NewUserRepository(database)
+	userService := service.NewUserService(userRepo)
+	http_app.NewUserHandler(router, userService)
+
+	log.Info("Run application")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
